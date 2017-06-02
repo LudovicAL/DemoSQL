82d62fe73e8b67f27409af28470f443c75c74088
Ludovic <ludovicaubut@hotmail.com>
2017-06-01 20:02:49 -0400

    no message

--- php/gestionRequete.php
+++ php/gestionRequete.php
@@ -11,22 +11,28 @@
 	}
 
 	//Retour d'un message complexe
-	function retourDonnees($infos, $action){
+	function retourDonnees($q, $infos, $action){
 		header ("Content-Type: text/xml");
 		echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
 		echo "<xml>\n";
 		echo "<action>$action</action>\n";
+		echo "<message>$q</message>";
+		$row = mysql_fetch_object($infos)
 		while ($row = mysql_fetch_object($infos)) {
-			echo "<membre>\n";
-			echo "<username>$row->username</username>\n";
-			echo "<password>$row->password</password>\n";
-			echo "</membre>\n";
+			echo "<item>\n";
+			echo "<organisation>$row->organisation</organisation>\n";
+			echo "<titre>$row->titre</titre>\n";
+			echo "<debut>$row->debut</debut>\n";
+			echo "<fin>$row->fin</fin>\n";
+			echo "<commentaire>$row->commentaire</commentaire>\n";
+			echo "</item>\n";
 		}
 		echo "</xml>";
 	}
 
 	//Identification de la requête à effectuer
-	switch ($_GET['requete']) {
+	$q=$_GET['requete'];
+	switch ($q) {
 		case "1":
 			$req="SELECT idemplois, compagnie, titre, debut, fin, description FROM `cvdata`.`emplois`;";
 			break;
@@ -55,21 +61,19 @@
 			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 			exit;
 			*/
-			retourMessage(mysqli_connect_error(), "echec");
-			exit;
 			die('Impossible de se connecter à la base de données.' . mysqli_error($con));
 		}
 		//Exécution de la requête SQL
 		mysqli_select_db($con, "cvdata");
 		$result = mysqli_query($con, $req);
 		if ($result->num_rows > 0) {
-			retourMessage("Plenty results", "echec");
+			retourDonnees($q, $result, "succes");
 		} else {
-			retourMessage("0 result", "echec");
+			retourMessage("La requête n'a pas retourné de résultats.", "echec");
 		}
 		//Fermeture de la base de données
 		mysqli_close($conn);
 	} catch (Exception $e) {
-		retourMessage("Une erreur est survenue. Veuillez reessayer.", "echec");
+		retourMessage("Une erreur est survenue. Veuillez réessayer.", "echec");
 	}
 ?>
